992f79d6426cd9f8066eee3a792cdc03f492bfab
Rajan <rajanvasani9@gmail.com>
2026-01-13 17:05:13 +0530

    fix: fixed issue with db connection due to missing adapter

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,11 +1,16 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
-
+import { PrismaPg } from '@prisma/adapter-pg'
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit
 {
+    constructor() {
+        const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
+        super({ adapter });
+    }
+    
   async onModuleInit() {
     await this.$connect();
   }
